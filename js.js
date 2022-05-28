afb5e164e28f5d81609e129de440585241d6e8f4
Davidmg25 <99227684+Davidmg25@users.noreply.github.com>
2022-05-28 13:32:41 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+
+
+let name2 =prompt("Decime tu nombre")
+
+alert("hola "+name2+ " Espero que te guste")
+
+let cambio= document.querySelectorAll(".azul");
+let BA=document.querySelector(".BA");
+function changeColor()
+{
+    if(BA.value="changeColor")
+    {
+        for(var i=0;i<cambio.length;i++)
+        {
+            cambio[i].style.backgroundColor="white";
+        }
+        console.log(BA)
+    }
+  
+ 
+    
+}
+
+let BA2=document.querySelector(".BA2")
+let camb=document.querySelectorAll(".azul")
+function changeColor2()
+{
+ if(BA2.value="changeColor2")
+ {
+    for(var i=0;i<camb.length;i++)
+    {
+        camb[i].style.backgroundColor="red";
+    }
+    console.log(BA2)
+ }
+}
+
+
+
